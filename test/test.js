aa72ff97a9f60485ed72ef799645bf593317d950
Shawn Elbaz <elbaz.shawn@gmail.com>
2019-08-09 03:48:30 -0400

    testing scoring

--- test/test.js
+++ test/test.js
@@ -1,58 +1,65 @@
-import chai from 'chai';
-import chatHttp from 'chai-http';
 import 'chai/register-should';
-import {app} from '../src/app';
 var assert = require('assert');
 import {calculateDistance, getPopulationPercentage, getSuggestionScore} from '../src/server/lib/suggestions';
-var expect = chai.expect;
 
-chai.use(chatHttp);
 
 describe('Testing the suggestions endpoints:', () => {
     it('It should return a list of matching suggestions', (done) => {
-      const url = '?q=Londo&latitude=43.70011&longitude=-79.4163'
-      chai.request(app)
-        .get('/suggestions/' + url)
-        .end((err, res) => {
-          expect(res.status).to.equal(201);
-          expect(res.body.data).to.include({
-            
-                "suggestions": [
-                    {
-                        "name": "London, ON, CA",
-                        "latitude": 42.98339,
-                        "longitude": -81.23304,
-                        "score": 0.4217199760713868
-                    },
-                    {
-                        "name": "London, OH, US",
-                        "latitude": 39.88645,
-                        "longitude": -83.44825,
-                        "score": 0.283702296272092
-                    },
-                    {
-                        "name": "London, KY, US",
-                        "latitude": 37.12898,
-                        "longitude": -84.08326,
-                        "score": 0.26211646432439983
-                    },
-                    {
-                        "name": "New London, CT, US",
-                        "latitude": 41.35565,
-                        "longitude": -72.09952,
-                        "score": 0.21749554087608675
-                    },
-                    {
-                        "name": "New London, WI, US",
-                        "latitude": 44.39276,
-                        "longitude": -88.73983,
-                        "score": 0.21098522791906896
-                    }
-                ]
-            
-          });
-          done();
+        let suggestions = {
+            "suggestions": [
+                {
+                    "name": "Saskatoon, SK, CA",
+                    "latitude": 52.11679,
+                    "longitude": -106.63452,
+                    "score": 0.13638845371653102
+                },
+                {
+                    "name": "Fort Saskatchewan, AB, CA",
+                    "latitude": 53.71684,
+                    "longitude": -113.2187,
+                    "score": 0.08641837705744926
+                }
+            ]
+        };
+        let cityMatches = [];
+        cityMatches.push({id: 1,
+            name: "Saskatoon",
+            country:"CA",
+            admin1:"SK",
+            latitude: 52.11679,
+            longitude: -106.63452,
+            population: 198958});
+        cityMatches.push({
+            id: 2,
+            name: "Fort Saskatchewan",
+            country: "CA",
+            admin1: "AB",
+            latitude: 53.71684,
+            longitude: -113.2187,
+            population: 14957
         });
+        let suggestionsList = {};
+        suggestionsList.suggestions = [];
+
+        for(let city in cityMatches){
+            let cityStateCountry = cityMatches[city].name + ', ' + cityMatches[city].admin1 + ', ' + cityMatches[city].country;
+            let suggestionsObj = {
+            "name": cityStateCountry ,
+            "latitude": cityMatches[city].latitude,
+            "longitude": cityMatches[city].longitude,
+            "score": getSuggestionScore(cityMatches[city].name, latitude, longitude, parseFloat(cityMatches[city].latitude), parseFloat(cityMatches[city].longitude), cityMatches[city].population, lookupValue)
+            };
+            suggestionsList.suggestions.push(suggestionsObj);
+        }
+        var sortedList = suggestionsList.suggestions.sort((a, b) => parseFloat(b.score) - parseFloat(a.score));
+        if (sortedList.length>5){
+            sortedList = sortedList.slice(0, 5);
+        }
+        var finalList = {};
+        finalList.suggestions = [];
+        finalList.suggestions = sortedList
+       
+        assert.deepEqual(finalList, suggestions);
     })
 });
 
